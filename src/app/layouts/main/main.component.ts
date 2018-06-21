866c0940163c7ee4194e5c3a903c71217dd3eb0a
Bogdanov Ilya <bogdanov@comepay.ru>
2018-06-21 11:17:11 +0300

    Получаем данные в callback

--- src/app/layouts/main/main.component.ts
+++ src/app/layouts/main/main.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Http } from '@angular/http';
-import { Token } from './token';
+import {Component, OnInit} from '@angular/core';
+import {Http} from '@angular/http';
+import {Token} from './token';
 
 @Component({
   selector: 'app-main',
@@ -9,24 +9,34 @@ import { Token } from './token';
 })
 export class MainComponent implements OnInit {
 
-  token;
+  public token;
 
   constructor(private http: Http, private getToken: Token) {
     this.token = this.getToken.Token;
   }
 
-  ngOnInit() {
-    /* Получаем свои данные */
-    this.http.get('https://api.instagram.com/v1/users/self/media/recent/?access_token='+this.token)
+  /* Сервис который будет возращать результат в формате json */
+  serviceGetData(url, callback) {
+    this.http.get(url + this.token)
       .subscribe((data) => {
-        let result = data.json();
-        console.log('result', result);
-      })
+        callback(data.json());
+      });
   }
 
-  /* Поиск по локации */
-  location() {
-    // https://api.instagram.com/v1/locations/search?lat=48.858844&lng=2.294351&access_token=this.token
+  ngOnInit() {
+    /* Получаем свои данные */
+    this.serviceGetData('https://api.instagram.com/v1/users/self/media/recent/?access_token=', this.myData); // url, callback
+    this.serviceGetData('https://api.instagram.com/v1/locations/search?lat=48.858844&lng=2.294351&distance=500&access_token=', this.location)
+  }
+
+  /* Получили мои данные */
+  myData(data) {
+    console.log('data', data);
+  }
+
+  /* Поиск по локации с дистанцией 100м - default=500 */
+  location(users) {
+    console.log('users', users);
   }
 
 }
